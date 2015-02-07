88e8c292fba23ef1f9857da74bd37a93aa1b567f
Neil Berrow <nsberrow@Neils-MacBook-Pro.local>
2015-02-07 11:28:21 +0200

    Testing single.php

--- templates/single.php
+++ templates/single.php
@@ -16,7 +16,6 @@ get_header(); ?>
 			<?php while ( have_posts() ) : the_post(); ?>
 
 				<?php get_template_part( 'content', get_post_format() ); ?>
-				<?php twentythirteen_post_nav(); ?>
 				<?php comments_template(); ?>
 
 			<?php endwhile; ?>
@@ -24,5 +23,4 @@ get_header(); ?>
 		</div><!-- #content -->
 	</div><!-- #primary -->
 
-<?php get_sidebar(); ?>
 <?php get_footer(); ?>
