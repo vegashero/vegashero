b63a3ea1468bf569bb4e6c5d7f57cf192d2808ea
Neil Berrow <neil.berrow@hetzner.co.za>
2015-06-04 22:02:00 +0200

    Local

--- templates/tablebody-vegashero.php
+++ templates/tablebody-vegashero.php
@@ -1,4 +1,4 @@
 <tr>
   <td class="vh-casino"><img src="%1$s/%2$s.png" width="180px"></td>
-  <td><a href="http://www.affiliaterepublik.com/?affid=1311" class="vh-playnow">Play at %3$s</a></td>
+  <td><a href="%2$s" class="vh-playnow">Play at %3$s</a></td>
 </tr>
