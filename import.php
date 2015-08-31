10d238989cb84343279df4b473ddb5a2e55ed8f8
Charles Mulder <charles@nerdcorp.co.za>
2015-08-31 19:24:42 +0200

    development environ

--- import.php
+++ import.php
@@ -13,16 +13,9 @@ class Vegashero_Import
         add_action('init', array($this, 'registerTaxonomies'));
 
         // this action is scheduled in update.php
-        add_action('vegashero_import', array($this, 'import_games'));
+        add_action('vegashero_import_by_provider', array($this, 'importGamesForProvider'));
     }
 
-    private function _setOperators() {
-        $endpoint = sprintf('%s/vegasgod/operators', $this->_config->apiUrl);
-        $response = wp_remote_retrieve_body(wp_remote_get($endpoint));
-        $this->_operators = json_decode(json_decode($response), true);
-        // $this->_operators = array_slice(array_keys((array)$game), 6, -2);
-    }
-    
    // public function setPermalinkStructure() {
    //     global $wp_rewrite;
    //     $wp_rewrite->set_permalink_structure('/%postname%/');
@@ -76,7 +69,7 @@ class Vegashero_Import
         return get_posts($args);
     }
 
-    private function _insertNewGame($game, $operator) {
+    private function _insertNewGame($game) {
         // [id] => 6
         // [name] => wild witches
         // [provider] => netent
@@ -99,7 +92,6 @@ class Vegashero_Import
         $post_id = wp_insert_post($post);
         $category_id = $this->_getCategoryId(trim($game->category));
         $provider_id = $this->_getProviderId(trim($game->provider));
-        $operator_id = $this->_getOperatorId(trim($operator));
 
         $post_meta_game_id = add_post_meta($post_id, $this->_config->postMetaGameId, $game->id, true); // add post meta data
         $post_meta_game_src_id = add_post_meta($post_id, $this->_config->postMetaGameSrc, $game->src, true); // add post meta data
@@ -107,11 +99,9 @@ class Vegashero_Import
 
         $game_category_term_id = wp_set_object_terms($post_id, $category_id, $this->_config->gameCategoryTaxonomy); // link category and post
         $game_provider_term_id = wp_set_object_terms($post_id, $provider_id, $this->_config->gameProviderTaxonomy); // link provider and post
-        $game_operator_term_id = wp_set_object_terms($post_id, $operator_id, $this->_config->gameOperatorTaxonomy); // link operator and post
 
         $this->_groupTerms(array($category_id), $this->_config->gameCategoryTermGroupId, $this->_config->gameCategoryTaxonomy);
         $this->_groupTerms(array($provider_id), $this->_config->gameProviderTermGroupId, $this->_config->gameProviderTaxonomy);
-        $this->_groupTerms(array($operator_id), $this->_config->gameOperatorTermGroupId, $this->_config->gameOperatorTaxonomy);
     }
 
     private function _updateExistingPostMeta($existing, $game) {
@@ -131,33 +121,6 @@ class Vegashero_Import
             update_post_meta($existing->ID, $this->_config->postMetaGameTitle, sanitize_title(strtolower(trim($game->name))), $game_title);
         }
     }
-    
-    private function _getOperatorIds($operators) {
-        $operator_ids = array();
-        foreach($operators as $operator) {
-            $operator_id = $this->_getOperatorId(trim($operator));
-            array_push($operator_ids, $operator_id);
-        }
-        return $operator_ids;
-    }
-
-    private function _getOperatorId($operator) {
-        if( ! $operator_id = term_exists($operator, $this->_config->gameOperatorTaxonomy)){
-            $operator_id = wp_insert_category(
-                array(
-                    'cat_name' => $operator,
-                    'category_description' => 'Vegas Hero Game Operators',
-                    'category_nicename' => sanitize_title($operator),
-                    'taxonomy' => $this->_config->gameOperatorTaxonomy
-                ),
-                true
-            );
-        }  else {
-            $term_details = get_term_by('name', $operator, $this->_config->gameOperatorTaxonomy);
-            $operator_id = (int)$term_details->term_id;
-        }
-        return $operator_id;
-    }
 
     private function _updateStatus($existing, $new) {
         $new->status = $new->status ? 'publish' : 'draft';
@@ -168,31 +131,11 @@ class Vegashero_Import
         }
     }
 
-    private function _updateOperators($existing, $new, $operator) {
-        $update = false;
-        $operators = wp_get_post_terms($existing->ID, $this->_config->gameOperatorTaxonomy, array('fields' => 'names'));
-        if( ! in_array($operator, $operators) && $new->{$operator}) {
-            array_push($operators, $operator);
-            $update = true;
-        }  elseif(! $new->{$operator}) {
-            if(($key = array_search($operator, $operators)) !== false) {
-                unset($operators[$key]);
-                $update = true;
-            }
-        }
-        if($update) {
-            $operator_ids = $this->_getOperatorIds($operators);
-            $game_operator_term_id = wp_set_object_terms($existing->ID, $operator_ids, $this->_config->gameOperatorTaxonomy); 
-            $this->_groupTerms($operator_ids, $this->_config->gameOperatorTermGroupId, $this->_config->gameOperatorTaxonomy);
-        }
-    }
-
-    private function _updateExistingGame($existing, $new, $operator) {
+    private function _updateExistingGame($existing, $new) {
         $this->_updateStatus($existing, $new);
-        $this->_updateOperators($existing, $new, $operator);
     }
 
-    public function import_games($operator) {
+    public function importGamesForProvider($provider) {
         // $this->registerTaxonomies();
 
         // [id] => 6
@@ -207,13 +150,7 @@ class Vegashero_Import
         // [created] => 2015-03-20 11:36:22
         // [modified] => 2015-03-20 11:36:22
 
-        # first time importing games for this operator
-        if( ! term_exists($operator, $this->_config->gameOperatorTaxonomy)){ 
-            $endpoint = sprintf('%s/vegasgod/games/%s', $this->_config->apiUrl, $operator);
-        } else {
-            # get all games so we can remove operators
-            $endpoint = sprintf('%s/vegasgod/games/', $this->_config->apiUrl);
-        }
+        $endpoint = sprintf('%s/vegasgod/games/provider/%s', $this->_config->apiUrl, $provider);
         $response = wp_remote_retrieve_body(wp_remote_get($endpoint));
         $games = json_decode(json_decode($response));
 
@@ -229,9 +166,9 @@ class Vegashero_Import
                 }
 
                 if( ! $post_id) { // no existing post
-                    $this->_insertNewGame($game, $operator);
+                    $this->_insertNewGame($game);
                 } else { 
-                    $this->_updateExistingGame($post, $game, $operator);
+                    $this->_updateExistingGame($post, $game);
                     $this->_updateExistingPostMeta($post, $game);
                 }
             }
@@ -279,37 +216,6 @@ class Vegashero_Import
 
     }
 
-    private function _registerGameOperatorTaxonomy() {
-        $labels = array(
-            'name'              => 'Game Operators',
-            'singular_name'     => 'Game Operator',
-            'search_items'      => 'Search Game Operators',
-            'all_items'         => 'All Games Operators',
-            'edit_item'         => 'Edit Game Operator',
-            'update_item'       => 'Update Game Operator',
-            'add_new_item'      => 'Add New Game Operator',
-            'new_item_name'     => 'New Game Operator',
-            'menu_name'         => 'Game Operators',
-        );
-
-        $args = array(
-            'hierarchical'      => false,
-            'labels'            => $labels,
-            'show_ui'           => true,
-            'show_admin_column' => true,
-            'query_var'         => true,
-            // 'rewrite'           => true
-            'rewrite' => array(
-                'slug' => $this->_config->gameOperatorUrlSlug,
-                'with_front' => true
-            )
-        );
-
-        register_taxonomy( $this->_config->gameOperatorTaxonomy, array( $this->_config->customPostType ), $args );
-        register_taxonomy_for_object_type( $this->_config->gameOperatorTaxonomy, $this->_config->customPostType );
-
-    }
-
     private function _registerGameProviderTaxonomy() {
         $labels = array(
             'name'              => 'Game Providers',
@@ -343,7 +249,6 @@ class Vegashero_Import
     public function registerTaxonomies() {
         require_once ABSPATH . 'wp-admin/includes/taxonomy.php';
         $this->_registerGameCategoryTaxonomy();
-        $this->_registerGameOperatorTaxonomy();
         $this->_registerGameProviderTaxonomy();
         flush_rewrite_rules();
     }
@@ -363,7 +268,6 @@ class Vegashero_Import
             'taxonomies' => array(
                 'post_tag',
                 $this->_config->gameProviderTaxonomy,
-                $this->_config->gameOperatorTaxonomy,
                 $this->_config->gameCategoryTaxonomy
             ),
             'show_ui' => true,
