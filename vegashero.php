2097cf9631016269b469ddd7b8f3ceceac8128f5
Charles Mulder <charles@nerdcorp.co.za>
2016-02-04 20:08:46 +0200

    fixed update notifier

--- vegashero.php
+++ vegashero.php
@@ -10,41 +10,26 @@
  * License: GPL2
  */
 
-// this is the URL our updater / license checker pings. This should be the URL of the site with EDD installed
-define( 'EDD_SAMPLE_STORE_URL', 'http://vegashero.co' ); // you should use your own CONSTANT name, and be sure to replace it throughout this file
-
-// the name of your product. This should match the download name in EDD exactly
-define( 'EDD_SAMPLE_ITEM_NAME', 'Vegas Hero' ); // you should use your own CONSTANT name, and be sure to replace it throughout this file
-
-if( !class_exists( 'EDD_SL_Plugin_Updater' ) ) {
-	// load our custom updater
-	include( dirname( __FILE__ ) . '/updater.php' );
-}
-
-function edd_sl_sample_plugin_updater() {
-
-	// retrieve our license key from the DB
-	$license_key = trim( get_option( 'edd_sample_license_key' ) );
-
-	// setup the updater
-	$edd_updater = new EDD_SL_Plugin_Updater( EDD_SAMPLE_STORE_URL, __FILE__, array(
-		'version' 	=> '1.3.1', 				// current version number
-		'license' 	=> $license_key, 		// license key (used get_option above to retrieve from DB)
-		'item_name' => EDD_SAMPLE_ITEM_NAME, 	// name of this plugin
-		'author' 	=> 'Vegas Hero'  // author of this plugin
-	)
-);
-
-}
-add_action( 'admin_init', 'edd_sl_sample_plugin_updater', 0 );
 
 require_once( dirname( __FILE__ ) . '/config.php' );
-
-//require_once( dirname( __FILE__ ) . '/settings.php' );
-//$dashboard = new Vegashero_Settings();
+$config = Vegashero_Config::getInstance();
 
 require_once( dirname( __FILE__ ) . '/settings/dashboard.php' );
 $dashboard = Vegashero_Settings_Dashboard::getInstance();
+
+require_once( dirname(__FILE__) . '/updater.php' );
+$updater = new EDD_SL_Plugin_Updater($config->eddStoreUrl, __FILE__, 
+    array(
+        'version'   => '1.0',       // current version number
+        'license'   => $dashboard->getLicense(),    // license key (used get_option above to retrieve from DB)
+        'item_name'     => $config->eddItemName,    // name of this plugin
+        'author'    => 'Vegas Heroes', // author of this plugin
+        'url'           => home_url()
+    ) 
+);
+
+//require_once( dirname( __FILE__ ) . '/settings.php' );
+//$dashboard = new Vegashero_Settings();
 
 require_once( dirname( __FILE__ ) . '/settings/operators.php' );
 $operators = new Vegashero_Settings_Operators();
