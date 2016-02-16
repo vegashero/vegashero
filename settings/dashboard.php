3bf72362120465e391c39d80bfdacfc4bdb40093
Imre Petrohai <imrepetrohai@gmail.com>
2016-02-16 19:18:42 +0100

    added lobby game count setting to dashboard, untested as shortcode is not working

--- settings/dashboard.php
+++ settings/dashboard.php
@@ -94,6 +94,21 @@ class Vegashero_Settings_Dashboard
         $option_group = self::_getOptionGroup();
         $option_name = self::_getOptionName();
         register_setting($option_group, $option_name, array($this, 'sanitizeLicense'));
+        // lobby settings
+        add_settings_section(
+        'vegashero_lobbySettings_section', 
+        __( 'Lobby Settings', 'vhero' ), 
+        '', 
+        'lobbySettings'
+        );
+        add_settings_field( 
+        'vegashero_lobbyGamesPerPage', 
+        __( 'Number of games to show', 'vhero' ), 
+        array($this, 'vegashero_lobbyGamesPerPage_render'), 
+        'lobbySettings', 
+        'vegashero_lobbySettings_section' 
+        );
+        register_setting( 'lobbySettings', 'vegashero_settings' );
     }
 
     private function _getLicenseNonceName() {
@@ -133,6 +148,12 @@ class Vegashero_Settings_Dashboard
         // echo "<input name='".$name."[".$key."]' size='40' type='text' value='".get_option($name)."' />";
         // for single option
         echo "<input id='$name' name='$name' size='30' type='text' class='regular-text' value='".get_option($name)."' placeholder='enter your license key here' />";
+    }
+
+    public function vegashero_lobbyGamesPerPage_render() { 
+      $options = get_option( 'vegashero_settings' , 20 );   //default game count 20
+      echo "<input type='text' name='vegashero_settings[vegashero_lobbyGamesPerPage]' value='".$options['vegashero_lobbyGamesPerPage']."'>";
+      echo "<p class='description'>If your lobby is showing in 4 columns set the number of games to show to be multiples of 4. For example to display a 4x4 grid of games set the number to 16. Default value is 20.</p>";
     }
 
 
