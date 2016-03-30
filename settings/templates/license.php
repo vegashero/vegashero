7bfbf8f5ce5e2d4c8ce57f6dca0981b2b734be6e
Imre Petrohai <imrepetrohai@gmail.com>
2016-03-30 18:30:32 +0200

    removed wp plugin repo reference

--- settings/templates/license.php
+++ settings/templates/license.php
@@ -10,9 +10,9 @@
 <div class="helpinfo">
 	<h3 style="margin-top:0;">Like this plugin?</h3>
 
-	<p><a target="_blank" href="https://wordpress.org/plugins/vegashero-casino-affiliate-plugin/">Give it a 5 star rating</a> on WordPress.org.</p>
+	<!-- <p><a target="_blank" href="https://wordpress.org/plugins/vegashero-casino-affiliate-plugin/">Give it a 5 star rating</a> on WordPress.org.</p> -->
 
-	<p>Check us out on <a target="_blank" href="https://www.facebook.com/Vegas-Hero-666401236777909/">Facebook</a> and follow our <a target="_blank" href="https://twitter.com/Vegas_Hero">Twitter</a> feed. Connect with us and share your journey of being/becoming an igaming affiliate. We woud love to hear your story.</p>
+	<p>Check us out on <a target="_blank" href="https://www.facebook.com/Vegas-Hero-666401236777909/">Facebook</a> and follow our <a target="_blank" href="https://twitter.com/Vegas_Hero">Twitter</a> feed. Connect with us and share your journey of being/becoming an igaming affiliate. We woud love to hear your story and check out your website.</p>
 
 	<h3>Need support?</h3>
 	Email us: <a href="mailto:support@vegashero.co">support@vegashero.co</a></p>
